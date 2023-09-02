03019ec8296ef06e306c6459741a2c7012ad48fd
furkan-developer <68426648+furkan-developer@users.noreply.github.com>
2023-09-02 14:10:04 +0300

    Preparation

---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAX.Models
+{
+    public class ProductViewModel
+    {
+        public String Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
